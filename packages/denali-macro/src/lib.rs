3b5c95ab0bc5011574dfdf2c81075f7a95e3838d
JNetherwood-Imig <jacksonbnetherwoodimig@gmail.com>
2025-08-20 23:45:59 -0700

    fix merge conflicts

--- packages/denali-macro/src/lib.rs
+++ packages/denali-macro/src/lib.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use proc_macro::TokenStream;
+use quote::quote;
+
+#[proc_macro]
+pub fn gen_code(_input: TokenStream) -> TokenStream {
+    let protocols_dir = std::env::var("OUT_DIR").unwrap();
+    println!("Protocols directory is {}", protocols_dir);
+    quote! {}
 }
