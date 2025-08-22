580276d766afb774b30553915373aae97da996f3
JNetherwood-Imig <jacksonbnetherwoodimig@gmail.com>
2025-08-21 20:59:17 -0700

    feat(macro): multi-file discovery

--- packages/denali-macro/src/lib.rs
+++ packages/denali-macro/src/lib.rs
@@ -4,12 +4,13 @@ use std::{ffi::OsString, fs::File, path::PathBuf};
 
 use proc_macro::TokenStream;
 use quote::quote;
+use walkdir::WalkDir;
 
 #[proc_macro]
-pub fn wayland_protocol(input: TokenStream) -> TokenStream {
+pub fn wayland_protocols(input: TokenStream) -> TokenStream {
     let expr = syn::parse_macro_input!(input as syn::LitStr);
 
-    match gen_protocol_inner(expr) {
+    match gen_protocols_inner(expr) {
         Ok(stream) => stream,
         Err(err) => quote! {
             compile_error!("Failed to generate Wayland protocol: {err}", err = #err);
@@ -18,7 +19,7 @@ pub fn wayland_protocol(input: TokenStream) -> TokenStream {
     }
 }
 
-fn gen_protocol_inner(expr: syn::LitStr) -> Result<TokenStream, String> {
+fn gen_protocols_inner(expr: syn::LitStr) -> Result<TokenStream, String> {
     let path: OsString = expr.value().into();
     let path = if let Some(manifest_dir) = std::env::var_os("CARGO_MANIFEST_DIR") {
         let mut buf = PathBuf::from(manifest_dir);
@@ -28,12 +29,39 @@ fn gen_protocol_inner(expr: syn::LitStr) -> Result<TokenStream, String> {
         path.into()
     };
 
-    let file = File::open(&path).map_err(|_| "Failed to read Wayland protocol file: {}")?;
-    let protocol = protocol_parser::parse_protocol(file).map_err(|_| "Failed to parse Wayland protocol file")?;
-    let message = format!("Parsed protocol: {}", protocol.name);
+    let files = collect_files(&path)?;
+
+    let mut message = String::new();
+    for file in files {
+        let protocol = protocol_parser::parse_protocol(file)
+            .map_err(|_| "Failed to parse Wayland protocol file")?;
+        message.push_str(format!("Parsed protocol: {}\n", protocol.name).as_str());
+    }
 
     Ok(quote! {
         compile_error!(#message);
     }
     .into())
+}
+
+fn collect_files(path: &PathBuf) -> Result<Vec<File>, String> {
+    let mut files = Vec::<File>::new();
+    if path.is_file() {
+        let file = File::open(&path).map_err(|_| "Failed to read Wayland protocol file: {}")?;
+        files.push(file);
+    } else if path.is_dir() {
+        for path in WalkDir::new(path)
+            .into_iter()
+            .filter_map(Result::ok)
+            .map(|e| e.into_path())
+            .filter(|p| p.is_file() && p.extension().is_some_and(|ext| ext == "xml"))
+        {
+            let file = File::open(&path).map_err(|_| "Failed to read Wayland protocol file: {}")?;
+            files.push(file);
+        }
+    } else {
+        return Err("Expected path to be a file or directory".to_string());
+    }
+
+    Ok(files)
 }
--- packages/denali-wayland/src/lib.rs
+++ packages/denali-wayland/src/lib.rs
@@ -1,4 +1,2 @@
-denali_macro::wayland_protocol!(
-    "/home/gavin/Dev/rust/denali/target/debug/build/denali-wayland-81fe2c791d23fcdd/out/protocols/alpha-modifier-v1.xml"
-);
-const BLAH: &str = concat!(env!("OUT_DIR"), "/protocols/cursor-shape-v1.xml");
+const PROTOCOLS_DIR: &str = concat!(env!("OUT_DIR"), "/protocols");
+denali_macro::wayland_protocols!("/usr/share/wayland-protocols");
