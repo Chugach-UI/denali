5321a60c4ba5d1a30eb175de3a4d3cbaa6c60c27
JNetherwood-Imig <jacksonbnetherwoodimig@gmail.com>
2025-08-23 01:49:41 -0700

    fix: remove broken kde protocols

--- packages/denali-wayland/build.rs
+++ packages/denali-wayland/build.rs
@@ -15,7 +15,6 @@ pub fn main() {
     let wayland_commit = lock_lines[0].replace("wayland=", "");
     let wayland_protocols_commit = lock_lines[1].replace("wayland-protocols=", "");
     let wlr_protocols_commit = lock_lines[2].replace("wlr-protocols=", "");
-    let kde_protocols_commit = lock_lines[3].replace("kde-protocols=", "");
 
     let wayland_xml_path = format!(
         "https://gitlab.freedesktop.org/wayland/wayland/-/raw/{}/protocol/wayland.xml",
@@ -34,10 +33,6 @@ pub fn main() {
         "https://gitlab.freedesktop.org/wlroots/wlr-protocols/-/archive/{commit}/wlr-protocols-{commit}.tar.gz?path=unstable",
         commit = wlr_protocols_commit
     );
-    let kde_protocols_archive_path = format!(
-        "https://invent.kde.org/libraries/plasma-wayland-protocols/-/archive/{commit}/plasma-wayland-protocols-{commit}.tar.gz?path=src/protocols",
-        commit = kde_protocols_commit
-    );
 
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let protocols_path = Path::new(&out_dir).join("protocols");
@@ -55,11 +50,6 @@ pub fn main() {
         &client,
         protocols_path.as_path(),
         wlr_protocols_unstable_archive_path,
-    );
-    unpack_protocols_tar(
-        &client,
-        protocols_path.as_path(),
-        kde_protocols_archive_path,
     );
 
     let generated_code_path = Path::new(&out_dir).join("wayland_client_protocols.rs");
@@ -88,13 +78,6 @@ fn unpack_protocols_tar(client: &Client, protocols_path: &Path, archive_path: St
         "xdg-foreign-unstable-v1.xml",
         "xdg-shell-unstable-v5.xml",
         "xdg-shell-unstable-v6.xml",
-        "fullscreen-shell.xml",
-        "remote-access.xml",
-        "surface-extension.xml",
-        "text-input.xml",
-        "text-input-unstable-v2.xml",
-        "wayland-eglstream-controller.xml",
-        "screencast.xml",
     ];
 
     let bytes = client.get(archive_path).send().unwrap().bytes().unwrap();
