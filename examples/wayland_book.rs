15838fe6fa9d788bb05356f5203ed4ad2b7382a9
JNetherwood-Imig <jacksonbnetherwoodimig@gmail.com>
2025-09-02 23:57:28 -0700

    add(examples): (wip) wayland book implementation

---
+++ examples/wayland_book.rs
@@ -0,0 +1,68 @@
+use denali_core::{handler::Handler, Interface};
+use denali_wayland::{
+    display_connection::DisplayConnection,
+    protocol::wayland::{
+        wl_compositor::WlCompositor,
+        wl_registry::{WlRegistry, WlRegistryEvent},
+        wl_shm::{WlShm, WlShmEvent},
+    },
+};
+use frunk::Coprod;
+
+struct App {
+    registry: WlRegistry,
+    compositor: Option<WlCompositor>,
+    shm: Option<WlShm>,
+}
+
+impl App {
+    pub async fn run(mut self, connection: DisplayConnection) {
+        type Ev<'a> = Coprod!(WlRegistryEvent<'a>, WlShmEvent);
+        loop {
+            connection.handle_event::<Ev<'_>, _>(&mut self).await;
+        }
+    }
+}
+
+impl Handler<WlRegistryEvent<'_>> for App {
+    fn handle(
+        &mut self,
+        message: WlRegistryEvent<'_>,
+        object_id: denali_core::wire::serde::ObjectId,
+    ) {
+        _ = object_id;
+        if let WlRegistryEvent::Global(global) = message {
+            if global.interface == WlCompositor::INTERFACE {
+                self.compositor = Some(self.registry.bind(global.name, 6));
+            }
+            if global.interface == WlShm::INTERFACE {
+                self.shm = Some(self.registry.bind(global.name, 2));
+            }
+        }
+    }
+}
+
+impl Handler<WlShmEvent> for App {
+    fn handle(&mut self, message: WlShmEvent, object_id: denali_core::wire::serde::ObjectId) {
+        _ = message;
+        _ = object_id;
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let connection = DisplayConnection::new().unwrap();
+    let app = App {
+        registry: connection.display().registry(),
+        compositor: None,
+        shm: None,
+    };
+    app.run(connection).await;
+}
+
+fn randname(buf: &mut [u8]) {}
+
+fn allocate_shm_file() {
+    let mut name = [0u8; 8];
+    randname(&mut name);
+}
